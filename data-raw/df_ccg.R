1c8c0980432c52ea34ca8c333bc5516035e67d50
Kayoung Goffe <kayoung.goffe1@nhs.net>
2022-06-03 19:59:09 +0100

    add ccg

--- data-raw/df_ccg.R
+++ data-raw/df_ccg.R
@@ -1,1 +1,11 @@
 # Read CCG and pivot longer
+
+ccg <- read.csv("./data-raw/ccg.csv", check.names = FALSE)
+
+ccg <- ccg |> 
+  tidyr::pivot_longer(
+    cols = !c(REGION:CCG_CODE),
+    names_to = c("METRIC", "YEAR_MONTH"),
+    names_sep = "-",
+    values_to = "VALUE"
+  )
